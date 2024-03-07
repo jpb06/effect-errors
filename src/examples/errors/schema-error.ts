f355bf8ab654d82bfb51a1934f1ce3f7d8ba0156
jpb06 <jpb.06@outlook.com>
2024-03-07 13:55:46 +0100

    chore: adding an errors section to the readme

---
+++ src/examples/errors/schema-error.ts
@@ -0,0 +1,8 @@
+import * as Schema from '@effect/schema/Schema';
+
+export class SchemaError extends Schema.TaggedError<SchemaError>()(
+  'SomethingBad',
+  {
+    cause: Schema.optional(Schema.unknown),
+  },
+) {}
