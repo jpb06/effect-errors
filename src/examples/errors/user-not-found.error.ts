8e527344f975fa58f314fb5f1d4c08669f47db96
jpb06 <jpb.06@outlook.com>
2024-03-07 11:43:40 +0100

    chore: simplifying ctor

--- src/examples/errors/user-not-found.error.ts
+++ src/examples/errors/user-not-found.error.ts
@@ -3,8 +3,5 @@ import { TaggedError } from 'effect/Data';
 export class UserNotFoundError extends TaggedError('UserNotFound') {
   constructor(readonly error?: unknown) {
     super();
-    if (error instanceof Error) {
-      this.error = error;
-    }
   }
 }
