1d4c733373cfac00e91f72a837e0f8abec9eb17f
jpb06 <jpb.06@outlook.com>
2024-03-07 11:25:59 +0100

    chore: example without spans

---
+++ src/examples/without-spans.ts
@@ -0,0 +1,32 @@
+import { Effect } from 'effect';
+import { readJson } from 'fs-extra';
+
+import { runPromise } from '../runners/run-promise';
+
+import { FetchError } from './errors/fetch-error';
+import { filename } from './util/filename.effect';
+
+const readUser = Effect.tryPromise(() => readJson('cool.ts'));
+
+const fetchTask = (userId: string) =>
+  Effect.tryPromise({
+    try: () => fetch(`https://yolo-bro-oh-no.org/users/${userId}`),
+    catch: (e) => new FetchError(e as never),
+  });
+
+const unwrapResponseTask = (response: Response) =>
+  Effect.tryPromise({
+    try: () => response.json(),
+    catch: (e) => new FetchError(e as never),
+  });
+
+const mainTask = Effect.gen(function* (_) {
+  yield* _(filename(__filename));
+
+  const { id } = yield* _(readUser);
+  const response = yield* _(fetchTask(id));
+
+  return yield* _(unwrapResponseTask(response));
+});
+
+runPromise(mainTask);
