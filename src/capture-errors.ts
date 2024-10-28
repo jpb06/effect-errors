db704ad420e208187c914b4dd99723c2b0cf1f79
jpb06 <jpb.06@outlook.com>
2024-10-28 22:21:48 +0100

    chore(capture errors): returning start and end time in spans data

--- src/capture-errors.ts
+++ src/capture-errors.ts
@@ -11,6 +11,8 @@ export interface ErrorSpan {
   name: string;
   attributes: Record<string, unknown>;
   durationInMilliseconds: number | undefined;
+  startTime: bigint;
+  endTime: bigint | undefined;
 }
 
 export interface ErrorData {
